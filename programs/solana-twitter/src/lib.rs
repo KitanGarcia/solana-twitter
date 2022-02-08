65b9911148949cbbf6eef5acda20f17c1ff7837d
Kitan Garcia <kitangar@gmail.com>
2022-02-08 10:30:08 -0800

    add tweet struct to lib.rs

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -12,3 +12,11 @@ pub mod solana_twitter {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account] // Custom Rust attribute provided by anchor. Removes tons of boilerplate
+pub struct Tweet { // Rust struct that defines properties (no methods) of our tweet
+  pub author: Pubkey, // User who published this tweet
+  pub timestamp: i64,
+  pub topic: String, // Optional topic field that will appear on that topic's page
+  pub content: String, // Actual content of the tweet
+}
